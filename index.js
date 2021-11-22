e184b1c183ac74eb4797bca636eb360e3c0bebe7
djibadiawara <djiba.diawara97@gmail.com>
2021-11-22 16:31:59 -0500

    commit tree 7e65865bf6955050bc8e93504715e3e3bd8cf29b

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = "James"
+function upperCaseCustomerName(){
+    
+     customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+} 
+
+function overwriteBestCustomer(message){
+    bestCustomer = message
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "bob";
+  }
+
+
+
+
